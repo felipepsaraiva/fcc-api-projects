008dd129598fef670e9def7f1e2e7b3b9e4450c1
felipepsaraiva <felipepiressaraiva@gmail.com>
2019-05-11 12:32:23 -0300

    Fixing sqlite migration for production environments

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite');
 
-let config = null;
+let config = {};
 if (process.env.NODE_ENV == 'development')
-  config = { force: 'last' };
+  config.force = 'last';
 
 module.exports = sqlite.open('./apis.sqlite').then((db) => db.migrate(config));
