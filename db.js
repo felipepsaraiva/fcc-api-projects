c1273d86f321079916b209eb8aac9cf985dffc96
felipepsaraiva <felipepiressaraiva@gmail.com>
2019-05-11 00:27:12 -0300

    Updating dependencies

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 var dbUrl = process.env.DB_URL;
-mongoose.connect(dbUrl, { useMongoClient: true });
+mongoose.connect(dbUrl, { useNewUrlParser: true });
 
 module.exports = mongoose;
